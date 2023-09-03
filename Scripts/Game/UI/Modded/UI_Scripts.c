10c2cc4d649a6476516a0774701a713ca6abdac7
Njpatman <Njpatman@users.noreply.github.com>
2023-09-03 17:43:42 -0400

    Update for Group UI
    Group UI has not been tested in a multiplayer environment.

--- Scripts/Game/UI/Modded/UI_Scripts.c
+++ Scripts/Game/UI/Modded/UI_Scripts.c
@@ -6,7 +6,34 @@ class COAL_GUI: SCR_InfoDisplay
 	
 	//---------------------------------------------------------------------------------------------------------------------------------
 	protected TextWidget m_wBearing;
+	protected TextWidget Player0;
+	protected TextWidget Player1;
+	protected TextWidget Player2;
+	protected TextWidget Player3;
+	protected TextWidget Player4;
+	protected TextWidget Player5;
+	protected TextWidget Player6;
+	protected TextWidget Player7;
+	protected TextWidget Player8;
+	protected TextWidget Player9;
+	protected TextWidget Player10;
+	protected TextWidget Player11;
 	protected ImageWidget m_wCompass;
+	protected ImageWidget Status0;
+	protected ImageWidget Status1;
+	protected ImageWidget Status2;
+	protected ImageWidget Status3;
+	protected ImageWidget Status4;
+	protected ImageWidget Status5;
+	protected ImageWidget Status6;
+	protected ImageWidget Status7;
+	protected ImageWidget Status8;
+	protected ImageWidget Status9;
+	protected ImageWidget Status10;
+	protected ImageWidget Status11;
+	TextWidget tArray[] = {Player1,Player2,Player3,Player4,Player5,Player6,Player7,Player8,Player9,Player10,Player11};
+	ImageWidget iArray[] = {Status1,Status2,Status3,Status4,Status5,Status6,Status7,Status8,Status9,Status10,Status11};
+	private ref array<EEditableEntityLabel> m_RoleLabels = {};
 	private ProgressBarWidget StamBar = null;
 	private SCR_CharacterControllerComponent m_cCharacterController = null;
 	private bool stamBarVisible = false;
@@ -51,29 +78,133 @@ class COAL_GUI: SCR_InfoDisplay
         OnStaminaChange(m_cCharacterController.GetStamina());
 		
 		// Get local played entitys PlayerID.
-		int playerId = GetGame().GetPlayerManager().GetPlayerIdFromControlledEntity(character);
+		//int playerID = GetGame().GetPlayerManager().GetPlayerIdFromControlledEntity(character);
 		
-		// Groups may not initilize until a second after players locally controlled entity spawns, CallLater allows us to call it in exactly 1000 miliseconds after players locally controlled entity spawns.
-		//GetGame().GetCallqueue().CallLater(GroupDisplay, 1000, false, playerId);
+		//GroupDisplay(owner, playerID);
+
 	}
 	//---------------------------------------------------------------------------------------------------------------------------------
 	
 	// Functions used for the UI
 	
 	//---------------------------------------------------------------------------------------------------------------------------------
-	protected void GroupDisplay(int playerId)
-	{
-		// Method returns null, tested in coop world
-		//SCR_AIGroup group = SCR_AIGroup.Cast(GetGame().GetWorld().FindEntityByID(playerId));
-		//SCR_AIGroup group = GetGroup(playerId);
 
-     	//Print("Debug: " + group);
-    	//foreach (AIAgent agent : agents)
-    	//{    
-    	//    Print("Group Member: " + agent);
-    	//}
+	void GroupDisplay(IEntity owner, int playerID)
+	{
+		bool playerInGroup = SCR_AIGroup.Cast(owner).IsPlayerInGroup(playerID);
+		if (!playerInGroup) return;
+		
+		int groupCount = SCR_AIGroup.Cast(owner).GetAgentsCount();
+		array<int> groupPlayersID = SCR_AIGroup.Cast(owner).GetPlayerIDs();
+		int groupLeaderID = SCR_AIGroup.Cast(owner).GetLeaderID();
+		
+		PlayerManager pMan = GetGame().GetPlayerManager();
+		
+		if (!Player0) Player0 = TextWidget.Cast(m_wRoot.FindAnyWidget("Player0"));
+		if (!Status0) Status0 = ImageWidget.Cast(m_wRoot.FindAnyWidget("Status0"));
+		string groupLeaderName = pMan.GetPlayerName(groupLeaderID);
+		Player0.SetText(groupLeaderName);
+		
+		for(int i = 0; i<=groupCount; i++) 
+		{
+			int localGroupPlayerID = groupPlayersID[i];
+			
+			ImageWidget statusDisplay = iArray[i];
+			TextWidget playerNameDisplay = tArray[i];
+			
+			string playerName = pMan.GetPlayerName(localGroupPlayerID);
+			IEntity playerEntity = pMan.GetPlayerControlledEntity(localGroupPlayerID); 
+			
+			playerNameDisplay.SetText(playerName);
+			
+			m_ItemResource = Resource.Load(playerEntity.GetItemResourceName());
+            IEntitySource entitySource = m_ItemResource.GetResource().ToEntitySource();
+			
+			//IEntitySource entitySource = playerEntity.GetResource().ToEntitySource();
+			
+			EWeaponType playerWeaponType = CheckWeapons(entitySource);
+			
+			switch (playerWeaponType)
+			{
+				case EWeaponType.WT_RIFLE : 
+				{
+					m_RoleLabels.Insert(EEditableEntityLabel.ROLE_RIFLEMAN);
+					break;
+				}
+				case EWeaponType.WT_MACHINEGUN : 
+				{
+					m_RoleLabels.Insert(EEditableEntityLabel.ROLE_MACHINEGUNNER);
+					break;
+				}
+				case EWeaponType.WT_ROCKETLAUNCHER : 
+				{
+					m_RoleLabels.Insert(EEditableEntityLabel.ROLE_ANTITANK);
+					break;
+				}
+				case EWeaponType.WT_GRENADELAUNCHER : 
+				{
+					m_RoleLabels.Insert(EEditableEntityLabel.ROLE_GRENADIER);
+					break;
+				}
+				case EWeaponType.WT_SNIPERRIFLE : 
+				{
+					m_RoleLabels.Insert(EEditableEntityLabel.ROLE_SHARPSHOOTER);
+					break;
+				}
+				default: 
+				{
+					break;
+				}
+			}
+		}
 	}
 	
+	EWeaponType CheckWeapons(IEntitySource entitySource)
+	{
+		array<ref array<IEntityComponentSource>> weaponSlotComponents = {};
+		array<string> componentTypeArray = {"CharacterWeaponSlotComponent"};
+		int weaponSlotCount = SCR_BaseContainerTools.FindComponentSources(entitySource, componentTypeArray, weaponSlotComponents);
+		
+		array<IEntityComponentSource> weaponSlotComponentSources = weaponSlotComponents.Get(0);
+		
+		if (!weaponSlotComponentSources)
+		{
+			return;
+		}
+		
+		foreach	(IEntityComponentSource weaponSlotComponent : weaponSlotComponentSources)
+		{
+			ResourceName weaponPrefab;
+			if (weaponSlotComponent.Get("WeaponTemplate", weaponPrefab))
+			{
+				if (!weaponPrefab)
+				{
+					continue;
+				}
+				
+				IEntitySource weaponSource = SCR_BaseContainerTools.FindEntitySource(Resource.Load(weaponPrefab));
+				if (!weaponSource)
+				{
+					continue;
+				}
+				
+				IEntityComponentSource weaponComponentSource = SCR_BaseContainerTools.FindComponentSource(weaponSource, "WeaponComponent");
+				if (!weaponComponentSource)
+				{
+					continue;
+				}
+				
+				EWeaponType weaponType = null;
+				if (weaponComponentSource.Get("WeaponType", weaponType))
+				{
+					return weaponType;
+				}
+				return weaponType;
+				
+			}
+		}
+	}
+
 	protected void SetBearingSetCompass(TextWidget Bearing, ImageWidget Compass)
 	{
 		// Get current world
@@ -94,14 +225,14 @@ class COAL_GUI: SCR_InfoDisplay
 		Compass.SetRotation(-yaw);
 		
 		// Set Bearing so if yaw outputs 6 it'll read 006
-		string Bearing_add = "";
+		string bearingAdd = "";
 		// I love this.
-		if (yaw >= 0 & yaw < 10 ) { Bearing_add = "00"; }
-		if (yaw >= 10 & yaw < 100) { Bearing_add = "0"; }
-		string Bearing_STR = Bearing_add + (yaw.ToString());
+		if (yaw >= 0 & yaw < 10 ) { bearingAdd = "00"; }
+		if (yaw >= 10 & yaw < 100) { bearingAdd = "0"; }
+		string bearingSTR = bearingAdd + (yaw.ToString());
 		
 		// Set Bearing Text
-		Bearing.SetText(Bearing_STR);
+		Bearing.SetText(bearingSTR);
 	}
 	
 	// Slowly fades the opacity of the bar
