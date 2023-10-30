303afa7901c13fc34c7d09fa924a3870e8b43d10
Njpatman <Njpatman@users.noreply.github.com>
2023-10-30 10:45:21 -0400

    Fix for Error Code
    Fixed an error code and made some adjustments to the layout and script

--- Scripts/Game/UI/Modded/UI_Scripts.c
+++ Scripts/Game/UI/Modded/UI_Scripts.c
@@ -5,8 +5,12 @@ class COAL_GUI: SCR_InfoDisplay
 	// Variables used on/for UI Components
 	
 	//---------------------------------------------------------------------------------------------------------------------------------
+	private TextWidget Bearing = null;
+	private ImageWidget Compass = null;
+	private ProgressBarWidget StamBar = null;
 	private bool stamBarVisible = false;
-	private int frame = 45;
+	private int groupRefresh = 45;
+	private int widgetRefresh = 360;
 	//---------------------------------------------------------------------------------------------------------------------------------
 	
 	// Main loop that repeats every frame
@@ -14,13 +18,21 @@ class COAL_GUI: SCR_InfoDisplay
 	//---------------------------------------------------------------------------------------------------------------------------------
 	override protected void UpdateValues(IEntity owner, float timeSlice)
 	{
+		
+		super.UpdateValues(owner, timeSlice);
+		
 		// Get local enity the player is controlling at the moment.
 		SCR_ChimeraCharacter character = SCR_ChimeraCharacter.Cast(SCR_PlayerController.GetLocalControlledEntity());
 		
-		// Get image Widgets
-		TextWidget Bearing = TextWidget.Cast(m_wRoot.FindAnyWidget("Bearing"));
-		ImageWidget Compass = ImageWidget.Cast(m_wRoot.FindAnyWidget("Compass"));
-		ProgressBarWidget StamBar = ProgressBarWidget.Cast(m_wRoot.FindWidget("StamBar"));
+		//Refresh base widgets every 360 frames
+		if (widgetRefresh >= 360) {
+			widgetRefresh = 0;
+			Bearing = TextWidget.Cast(m_wRoot.FindAnyWidget("Bearing"));
+			Compass = ImageWidget.Cast(m_wRoot.FindAnyWidget("Compass"));
+			StamBar = ProgressBarWidget.Cast(m_wRoot.FindWidget("StamBar"));
+		} else {
+			widgetRefresh++;
+		};
 		
 		// Can't run if these dont exist better exit out.
 		if (!character || !Bearing || !Compass || !StamBar) return;
@@ -36,62 +48,55 @@ class COAL_GUI: SCR_InfoDisplay
 		// Use local Charachter Controller to get the current players stamina, then use custom function OnStaminaChange() to show current stamina on players screen.
         OnStaminaChange(m_cCharacterController.GetStamina(), StamBar);
 		
-		// Update group every 45 frames. (May lower to 30 depending on feedback from players with lower end PCs)
-		if (frame >= 45) {
+		// Update Group Display every 45 frames. (May lower to 30 depending on feedback from players with lower end PCs)
+		if (groupRefresh >= 45) {
+				
+			groupRefresh = 0;
 			
-			// Reset Frame Count
-			frame = 0;
-			
-			// Get Global Player Controller and Group Manager
+			// Get Global Player Controller and Group Manager.
 			SCR_PlayerController playerController = SCR_PlayerController.Cast(GetGame().GetPlayerController());
 			SCR_GroupsManagerComponent groupManager = SCR_GroupsManagerComponent.Cast(GetGame().GetGameMode().FindComponent(SCR_GroupsManagerComponent));
 			
+			// If for some reason the global player controller or group manager dont exist, better exit out. (and start praying to whatever god you beleve in)
 			if (!playerController || !groupManager) return;
 			
-			//Get list of all the player we have to parse through
-			array<int> groupPlayersIDs = groupManager.GetPlayerGroup(playerController.GetPlayerId()).GetPlayerIDs();
+			SCR_AIGroup playersGroup = groupManager.GetPlayerGroup(playerController.GetPlayerId());
+			
+			// If player doesn't have a group we can't do much, better check then clear their group display, just in case.
+			if (!playersGroup) {
+				ClearGroupDisplay(0, false);
+				return;
+			};
+			
+			// Get list of all the players we have to parse through.
+			array<int> groupPlayersIDs = playersGroup.GetPlayerIDs();
 			//array<int> groupPlayersIDs = {1,1,1,1,1,1}; //Debugging
 			
 			// Count how many players we plan on parsing through.
 			int groupCount = groupPlayersIDs.Count();
 			
-			// Get current group leader
-			int groupLeaderID = groupManager.GetPlayerGroup(playerController.GetPlayerId()).GetLeaderID();
+			//If person is only one in group, check then clear the group display.
+			if (groupCount == 1 || groupCount == 0) {
+				ClearGroupDisplay(0, false);
+				return;
+			};
 			
-			// Parse through groupPlayersIDs and show players names and icons on the group display
+			// Get current group leader.
+			int groupLeaderID = playersGroup.GetLeaderID();
+			
+			// Parse through groupPlayersIDs and show players names and icons on the group display.
 			GroupDisplay(groupLeaderID, groupCount, groupPlayersIDs);
 		} else {
-			// Add to Frame Count
-			frame++;
+			groupRefresh++;
 		};
-		
-		super.UpdateValues(owner, timeSlice);
 	}
 	//---------------------------------------------------------------------------------------------------------------------------------
 	
 	// Functions used for the UI
 	
 	//---------------------------------------------------------------------------------------------------------------------------------
-	void GroupDisplay(int groupLeaderID, int groupCount, array<int> groupPlayersIDs)
+	protected void GroupDisplay(int groupLeaderID, int groupCount, array<int> groupPlayersIDs)
 	{	
-		//If person is only one in group, clear the group display
-		if (groupCount == 1) {
-			for(int e = 0; e<=18; e++)
-			{
-				// Get group display widgets.
-				TextWidget playerRemoveDisplay = TextWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Player%1", e)));
-				ImageWidget statusRemoveDisplay = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", e)));
-				
-				// Skip ahead to next for loop iteration if either of these are false.
-				if (!playerRemoveDisplay || !statusRemoveDisplay) continue;
-				
-				// Clear widgets.
-				playerRemoveDisplay.SetText("");
-				statusRemoveDisplay.SetOpacity(0);
-			}
-			return;
-		};
-		
 		// Cannot guarantee that the player at 0 in groupPlayersIDs will always be the squad leader, so we find him in the group array and force him there.
 		int leaderIndex = groupPlayersIDs.Find(groupLeaderID);
 		groupPlayersIDs.Remove(leaderIndex);
@@ -99,7 +104,7 @@ class COAL_GUI: SCR_InfoDisplay
 		// Sort the array from player IDs. (typically comes out as the first to last players on the squad to connect)
 		groupPlayersIDs.Sort(true);
 		
-		// Push the Squad leader to the first Icon
+		// Push the Squad leader to the first Icon.
 		groupPlayersIDs.InsertAt(groupLeaderID, 0);
 		
 		// Parse through current group array.
@@ -116,7 +121,7 @@ class COAL_GUI: SCR_InfoDisplay
 			TextWidget playerDisplay = TextWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Player%1", i)));
 			ImageWidget statusDisplay = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", i)));
 			
-			// Skip ahead to next for loop iteration if any of whese are false.
+			// Skip ahead to next for-loop iteration if any of whese are false.
 			if (!statusDisplay || !playerDisplay || !playerName || !localplayer) continue;
 			
 			// Set player text.
@@ -164,7 +169,7 @@ class COAL_GUI: SCR_InfoDisplay
 							break;
 						}
 					}
-					// Skip ahead to next for loop iteration since we've done our job of setting the target players icon.
+					// Skip ahead to next for-loop iteration since we've done our job of setting the target players icon.
 					continue;
 			    };
 			};
@@ -179,7 +184,7 @@ class COAL_GUI: SCR_InfoDisplay
 				// Set Squad Leader Icon
 				statusDisplay.SetOpacity(1);
 				statusDisplay.LoadImageTexture(0, "{5ECE094ED4662B33}Layouts\UI\Textures\Icons\iconmanleader_ca.edds");
-				// Skip ahead to next for loop iteration since we've done our job of setting the target players icon.
+				// Skip ahead to next for-loop iteration since we've done our job of setting the target players icon.
 				continue;
 			};
 			
@@ -195,7 +200,6 @@ class COAL_GUI: SCR_InfoDisplay
 			array<EWeaponType> WeaponTypeArray = new array<EWeaponType>();
 			array<SCR_EConsumableType> MedicalTypeArray = new array<SCR_EConsumableType>();
 			array<BaseMuzzleComponent> muzzles = {};
-			
 			EntityPrefabData prefabData;
 			string resourceName;
 			
@@ -224,7 +228,6 @@ class COAL_GUI: SCR_InfoDisplay
 						MedicalTypeArray.Insert(medicalType);
 					};
 				};
-				
 				// Check if item is a Weapon.
 				WeaponComponent weaponComp = WeaponComponent.Cast(item.FindComponent(WeaponComponent));
 				if (weaponComp) {
@@ -250,7 +253,6 @@ class COAL_GUI: SCR_InfoDisplay
 					}
 				};
 			};
-			
 			// Take all the data we just collected and assign players a icon based on if it exists in the weapon/medical arrays.
 			switch (true) 
 			{
@@ -291,24 +293,33 @@ class COAL_GUI: SCR_InfoDisplay
 				};
 			};
 		}
-		
 		// Clear the group display bellow what we just populated, getting rid of any defunct player names/icons.
-		for(int e = groupCount; e<=18; e++) 
-		{
-			// Get group display widgets.
-			TextWidget playerRemoveDisplay = TextWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Player%1", e)));
-			ImageWidget statusRemoveDisplay = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", e)));
-			
-			// Skip ahead to next for loop iteration if either of these are false.
-			if (!playerRemoveDisplay || !statusRemoveDisplay) continue;
-			
-			// Clear widgets.
-			playerRemoveDisplay.SetText("");
-			statusRemoveDisplay.SetOpacity(0);
-		}
+		ClearGroupDisplay(groupCount, true);
 	}
+	
+	protected void ClearGroupDisplay(int positionToStartClearing, bool forceClear)
+	{
+		//Check if there's anything to clear
+		ImageWidget DisplayCheck = ImageWidget.Cast(m_wRoot.FindAnyWidget("Status0"));
+		int Check = DisplayCheck.GetOpacity();
+		if (Check == 1 || forceClear) {
+			for(int e = positionToStartClearing; e<=18; e++)
+			{
+				// Get group display widgets.
+				TextWidget playerRemoveDisplay = TextWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Player%1", e)));
+				ImageWidget statusRemoveDisplay = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", e)));
+					
+				// Skip ahead to next for-loop iteration if either of these are false.
+				if (!playerRemoveDisplay || !statusRemoveDisplay) continue;
+					
+				// Clear widgets.
+				playerRemoveDisplay.SetText("");
+				statusRemoveDisplay.SetOpacity(0);
+			}
+		};
+	};
 
-	protected void SetBearingSetCompass(TextWidget Bearing, ImageWidget Compass)
+	protected void SetBearingSetCompass(TextWidget BearingRef, ImageWidget CompassRef)
 	{
 		// Get current world
 		World world = GetGame().GetWorld();
@@ -325,7 +336,7 @@ class COAL_GUI: SCR_InfoDisplay
 		if (yaw < 0) { yaw = 360 - Math.AbsFloat(yaw); }
 		
 		// Rotate Compass texture to current bearing direction (yaw) 
-		Compass.SetRotation(-yaw);
+		CompassRef.SetRotation(-yaw);
 		
 		// Set Bearing so if yaw outputs 6 it'll read 006
 		string bearingAdd = "";
@@ -335,7 +346,7 @@ class COAL_GUI: SCR_InfoDisplay
 		string bearingSTR = bearingAdd + (yaw.ToString());
 		
 		// Set Bearing Text
-		Bearing.SetText(bearingSTR);
+		BearingRef.SetText(bearingSTR);
 	}
 	
 	// Slowly fades the opacity of the bar
@@ -357,19 +368,19 @@ class COAL_GUI: SCR_InfoDisplay
 	{
 		stamBarVisible = true;
 		widget.SetOpacity(0.5);
-		//fadeBar(StamBar, 0.5, 2);
+		//fadeBar(StamBarRef, 0.5, 2);
 	}
 	
 	protected void hideBar(ProgressBarWidget widget) 
 	{
 		stamBarVisible = false;
 		widget.SetOpacity(0);
-		//fadeBar(StamBar, 0, 2);
+		//fadeBar(StamBarRef, 0, 2);
 	}
 	
-	void OnStaminaChange(float value, ProgressBarWidget StamBar)
+	void OnStaminaChange(float value, ProgressBarWidget StamBarRef)
     {
-		StamBar.SetCurrent(value);
+		StamBarRef.SetCurrent(value);
 		//Print("Stamina: " + value);
 		//Print("stamBarVisible: " + stamBarVisible);
 		
@@ -377,22 +388,22 @@ class COAL_GUI: SCR_InfoDisplay
 		// I couldnt get the switch statement to work. I hate this.
 		if (value < 0.7 && value > 0.3)
 		{
-			StamBar.SetColor(new Color(0.091997,0.083009,0.035996,1.000000));
+			StamBarRef.SetColor(new Color(0.091997,0.083009,0.035996,1.000000));
 		} else if (value < 0.3) 
 		{
-			StamBar.SetColor(new Color(0.188724,0.046860,0.037476,1.000000));
+			StamBarRef.SetColor(new Color(0.188724,0.046860,0.037476,1.000000));
 		} else // Aka above 70%
 		{
-			StamBar.SetColor(new Color(0.035996,0.091997,0.050004,1.000000));
+			StamBarRef.SetColor(new Color(0.035996,0.091997,0.050004,1.000000));
 		}
 		
 		// Opacity
 		if (value < 1 && !stamBarVisible) 
 		{
-			revealBar(StamBar);
+			revealBar(StamBarRef);
 		} else if (value == 1 && stamBarVisible) 
 		{
-			hideBar(StamBar);
+			hideBar(StamBarRef);
 		}
     }
 };
