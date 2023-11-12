ce72dad9be30a1a66aeecc8bb6a3f43583000c69
Njpatman <Njpatman@users.noreply.github.com>
2023-11-11 19:58:16 -0500

    Color Teams Release
    Release for Color Teams

--- Scripts/Game/UI/Modded/COA_CompassUI.c
+++ Scripts/Game/UI/Modded/COA_CompassUI.c
@@ -2,145 +2,156 @@ class COA_Compass : SCR_InfoDisplay
 {
 	private TextWidget Bearing = null;
 	private ImageWidget Compass = null;
-	private bool isVisible = true;
+	private bool CompassVisible = true;
 	private string groupStringStored = "N/A";
 	private int groupRefresh = 30;
-	
+
 	override protected void OnInit(IEntity owner)
 	{
 		super.OnInit(owner);
 		GetGame().GetInputManager().AddActionListener("ToggleCompass", EActionTrigger.DOWN, ToggleCompass);
-
 	}
-	
+
 	override protected void UpdateValues(IEntity owner, float timeSlice)
 	{
-		
 		super.UpdateValues(owner, timeSlice);
-		
+
 		// Get local enity the player is controlling at the moment.
 		SCR_ChimeraCharacter character = SCR_ChimeraCharacter.Cast(SCR_PlayerController.GetLocalControlledEntity());
-		
+
 		//Refresh base widgets if we can't get them
 		if (!Bearing || !Compass) {
 			Bearing = TextWidget.Cast(m_wRoot.FindAnyWidget("Bearing"));
 			Compass = ImageWidget.Cast(m_wRoot.FindAnyWidget("Compass"));
 		};
-		
+
 		// Can't run if these dont exist better exit out.
 		if (!character || !Bearing || !Compass) return;
-	
+
 		// Sets Bearings text and the Compass direction
 		SetBearingSetCompass(Bearing, Compass);
-		
-		// Get Charachter Controller of the locally played entity.
-		SCR_CharacterControllerComponent m_cCharacterController = SCR_CharacterControllerComponent.Cast(character.FindComponent(SCR_CharacterControllerComponent));
-		
-		if (!m_cCharacterController ) return;
 
 		// Update Group Display every 35 frames.
-		if (groupRefresh >= 35) {	
+		if (groupRefresh >= 35) {
 			groupRefresh = 0;
 			// Get Global Player Controller and Group Manager.
 			SCR_PlayerController playerController = SCR_PlayerController.Cast(GetGame().GetPlayerController());
 			SCR_GroupsManagerComponent groupManager = SCR_GroupsManagerComponent.Cast(GetGame().GetGameMode().FindComponent(SCR_GroupsManagerComponent));
-			
+
 			// If for some reason the global player controller or group manager dont exist, better exit out. (and start praying to whatever god you beleve in)
 			if (!playerController || !groupManager) return;
-			
+
 			SCR_AIGroup playersGroup = groupManager.GetPlayerGroup(playerController.GetPlayerId());
-			
+
 			// If player doesn't have a group we can't do much, better check then clear their group display, just in case.
-			if (!playersGroup) {ClearGroupDisplay(0, false); return;};
-			
+			if (!playersGroup) {ClearGroupDisplay(0, false); return; };
+
 			string groupString = groupManager.ReturnGroupMapValue(playersGroup.ToString());
-			
-			if (!groupString || groupString == "") {ClearGroupDisplay(0, false); return;};
-			
+
+			if (!groupString || groupString == "") {ClearGroupDisplay(0, false); return; };
+
 			if (groupString == groupStringStored) return;
-			
 			groupStringStored = groupString;
-			
+
 			array<string> localGroupSplitString = {};
 			groupString.Split(";", localGroupSplitString, true);
-			
+
 			int groupCount = localGroupSplitString.Count();
-			groupCount = groupCount/3;
-			
+			groupCount = groupCount/ 4;
+
 			int playerPlace = 0;
 
 			// Parse through current group array.
-			for(int i = 0; i<groupCount && i < 12; i++) 
+			for (int i = 0; i<groupCount && i < 12; i++)
 			{
 				string playerName = localGroupSplitString[playerPlace];
 				string colorTeam = localGroupSplitString[playerPlace + 1];
 				string icon = localGroupSplitString[playerPlace + 2];
-				
-				playerPlace = playerPlace + 3;
-			
+
+				playerPlace = playerPlace + 4;
+
 				// Get group display widgets.
 				TextWidget playerDisplay = TextWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Player%1", i)));
 				ImageWidget statusDisplay = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", i)));
-				
+
 				playerDisplay.SetText(playerName);
 				statusDisplay.SetOpacity(1);
 				statusDisplay.LoadImageTexture(0, icon);
 				
-				if (colorTeam) {
-					switch (colorTeam) 
+				float sx = 0;
+				float yx = 0;
+				playerDisplay.GetTextSize(sx, yx);
+			
+				if (sx > 145) {
+			  		for (int e = 0; sx > 138.5; e++)
 					{
-						case "Red"    : {statusDisplay.SetColorInt(ARGB(255, 230, 0, 0));     playerDisplay.SetColorInt(ARGB(255, 230, 0, 0));     break;};
-						case "Blue"   : {statusDisplay.SetColorInt(ARGB(255, 0, 92, 255));    playerDisplay.SetColorInt(ARGB(255, 0, 92, 255));    break;};
-						case "Yellow" : {statusDisplay.SetColorInt(ARGB(255, 230, 230, 0));   playerDisplay.SetColorInt(ARGB(255, 230, 230, 0));   break;};
-						case "Green"  : {statusDisplay.SetColorInt(ARGB(255, 0, 230, 0));     playerDisplay.SetColorInt(ARGB(255, 0, 230, 0));     break;};
-						case "None"   : {statusDisplay.SetColorInt(ARGB(255, 255, 255, 255)); playerDisplay.SetColorInt(ARGB(255, 255, 255, 255)); break;};
+						int playerNameLength = playerName.Length();
+						playerNameLength = playerNameLength - 1;
+						playerName = playerName.Substring(0, playerNameLength);
+						
+						//Players display wont visually update until the UpdateValues function ends, so they don't see this.
+						playerDisplay.SetText(playerName);
+						playerDisplay.GetTextSize(sx, yx);
+					};
+					playerName = string.Format("%1...", playerName);
+					playerDisplay.SetText(playerName);
+				};
+
+				if (colorTeam) {
+					switch (colorTeam)
+					{
+						case "Red"    : {statusDisplay.SetColorInt(ARGB(255, 200, 65, 65));   playerDisplay.SetColorInt(ARGB(255, 200, 65, 65));   break; };
+						case "Blue"   : {statusDisplay.SetColorInt(ARGB(255, 0, 92, 255));    playerDisplay.SetColorInt(ARGB(255, 0, 92, 255));    break; };
+						case "Yellow" : {statusDisplay.SetColorInt(ARGB(255, 230, 230, 0));   playerDisplay.SetColorInt(ARGB(255, 230, 230, 0));   break; };
+						case "Green"  : {statusDisplay.SetColorInt(ARGB(255, 0, 190, 85));    playerDisplay.SetColorInt(ARGB(255, 0, 190, 85));    break; };
+						case "None"   : {statusDisplay.SetColorInt(ARGB(255, 255, 255, 255)); playerDisplay.SetColorInt(ARGB(255, 255, 255, 255)); break; };
 					};
 				};
-			};	
+			};
 			ClearGroupDisplay(groupCount, true);
 		} else {
 			groupRefresh++;
 		};
 	}
-	
-	//---------------------------------------------------------------------------------------------------------------------------------
-	
+
+	//------------------------------------------------------------------------------------------------
+
 	// Group Display Additional Functions
-	
-	//---------------------------------------------------------------------------------------------------------------------------------
-	
+
+	//------------------------------------------------------------------------------------------------
+
 	protected void ClearGroupDisplay(int positionToStartClearing, bool forceClear)
 	{
 		//Check if there's anything to clear
 		ImageWidget DisplayCheck = ImageWidget.Cast(m_wRoot.FindAnyWidget("Status0"));
 		int Check = DisplayCheck.GetOpacity();
 		if (Check == 1 || forceClear) {
-			for(int e = positionToStartClearing; e<=18; e++)
+			for (int e = positionToStartClearing; e<=12; e++)
 			{
 				// Get group display widgets.
 				TextWidget playerRemoveDisplay = TextWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Player%1", e)));
 				ImageWidget statusRemoveDisplay = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", e)));
-					
+
 				// Skip ahead to next for-loop iteration if either of these are false.
 				if (!playerRemoveDisplay || !statusRemoveDisplay) continue;
-							
+
 				// Clear widgets.
 				playerRemoveDisplay.SetText("");
 				statusRemoveDisplay.SetOpacity(0);
 			}
 		};
 	};
-	
-	//---------------------------------------------------------------------------------------------------------------------------------
-	
+
+	//------------------------------------------------------------------------------------------------
+
 	// Compass Functions
-	
-	//---------------------------------------------------------------------------------------------------------------------------------
-	
+
+	//------------------------------------------------------------------------------------------------
+
 	protected void ToggleCompass()
 	{
-		isVisible = !isVisible;
-		if(isVisible)
+		CompassVisible = !CompassVisible;
+		if (CompassVisible)
 		{
 			Compass.SetOpacity(1);
 			Bearing.SetOpacity(1);
@@ -150,7 +161,6 @@ class COA_Compass : SCR_InfoDisplay
 			Compass.SetOpacity(0);
 			Bearing.SetOpacity(0);
 		}
-		
 	}
 
 	protected void SetBearingSetCompass(TextWidget BearingRef, ImageWidget CompassRef)
@@ -158,27 +168,27 @@ class COA_Compass : SCR_InfoDisplay
 		// Get current world
 		World world = GetGame().GetWorld();
 		if (!world) return;
-		
+
 		// Settup transform
 		vector transform[4];
 		// Get current vector camera is looking at
 		world.GetCurrentCamera(transform);
-		
+
 		// Creates rotation matrix from angles (yaw, pitch, roll in degrees)
 		int yaw = Math3D.MatrixToAngles(transform)[0];
 		// Returns absolute value of yaw
 		if (yaw < 0) { yaw = 360 - Math.AbsFloat(yaw); }
-		
-		// Rotate Compass texture to current bearing direction (yaw) 
+
+		// Rotate Compass texture to current bearing direction (yaw)
 		CompassRef.SetRotation(-yaw);
-		
+
 		// Set Bearing so if yaw outputs 6 it'll read 006
 		string bearingAdd = "";
 		// I love this.
-		if (yaw >= 0 & yaw < 10 ) { bearingAdd = "00"; }
+		if (yaw >= 0 & yaw < 10) { bearingAdd = "00"; }
 		if (yaw >= 10 & yaw < 100) { bearingAdd = "0"; }
 		string bearingSTR = bearingAdd + (yaw.ToString());
-		
+
 		// Set Bearing Text
 		BearingRef.SetText(bearingSTR);
 	}
---
+++ Scripts/Game/UI/Modded/COA_PlayerSelectionUI.c
@@ -0,0 +1,174 @@
+modded enum ChimeraMenuPreset {
+	COA_PlayerSettingsUI,
+	COA_PlayerSelectionUI
+}
+
+modded class GroupSettingsDialogUI : DialogUI
+{
+	override void OnMenuOpen()
+	{
+		super.OnMenuOpen();
+		SCR_ButtonBaseComponent.GetButtonBase("ColorTeams", GetRootWidget()).m_OnClicked.Insert(CTButtonClicked);
+	}
+	
+	protected void CTButtonClicked()
+	{
+		GetGame().GetMenuManager().CloseMenu(this);
+		GetGame().GetMenuManager().OpenMenu(ChimeraMenuPreset.COA_PlayerSelectionUI);
+	}
+}
+
+class COA_PlayerSelectionUI : ChimeraMenuBase
+{
+	protected Widget m_wRoot;
+	protected InputManager m_InputManager;
+	protected string groupStringStored = "N/A";
+	
+	protected int PlayerInt;
+	protected int SelectedPlayerID = 0;
+	
+	protected SCR_PlayerController playerController = null;
+	protected SCR_GroupsManagerComponent groupManager = null;
+	protected SCR_AIGroup playersGroup = null;
+	
+	override void OnMenuShow() 
+	{
+		UpdatePlayerList();
+	}
+	
+	override void OnMenuInit()
+	{
+		m_wRoot = GetRootWidget();
+		
+		m_InputManager = GetGame().GetInputManager();
+		m_InputManager.AddActionListener("MenuBack", EActionTrigger.DOWN, OnMenuBack);
+		SCR_ButtonBaseComponent.GetButtonBase("Back", m_wRoot).m_OnClicked.Insert(OnMenuBack);
+
+		// Get Global Player Controller and Group Manager.
+		playerController = SCR_PlayerController.Cast(GetGame().GetPlayerController());
+		groupManager = SCR_GroupsManagerComponent.Cast(GetGame().GetGameMode().FindComponent(SCR_GroupsManagerComponent));
+
+		if (!playerController || !groupManager) {
+			GetGame().GetInputManager().RemoveActionListener("MenuBack", EActionTrigger.DOWN, OnMenuBack);
+			GetGame().GetMenuManager().CloseMenu(this);
+			return;
+		};
+		
+		playersGroup = groupManager.GetPlayerGroup(playerController.GetPlayerId());
+		
+		array<int> groupPlayersIDs = playersGroup.GetPlayerIDs();
+		if (groupPlayersIDs.Count() == 1 || groupPlayersIDs.Count() == 0) {
+			GetGame().GetMenuManager().CloseMenu(this);
+			return;
+		};
+		
+		for (int b = 0; b < 12; b++)
+		{
+			SCR_ButtonBaseComponent.GetButtonBase(string.Format("Button%1", b), m_wRoot).m_OnClicked.Insert(OnPlayerEntryClicked);
+		};
+	}
+	
+	protected void OnMenuBack()
+	{
+		GetGame().GetInputManager().RemoveActionListener("MenuBack", EActionTrigger.DOWN, OnMenuBack);
+		GetGame().GetMenuManager().CloseMenu(this);
+	}
+		
+	protected void OnPlayerEntryClicked(SCR_ButtonBaseComponent PEcomponent)
+	{
+		string PlayerIntStr = PEcomponent.GetRootWidget().GetName();
+		
+		PlayerInt = PlayerIntStr.ToInt(offset: 6);
+		
+		ImageWidget statusDisplayList = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", PlayerInt)));
+		int playerOInt = statusDisplayList.GetOpacity();
+		if (!playerOInt || playerOInt == 0) return;
+
+		MenuBase menu = GetGame().GetMenuManager().OpenMenu(ChimeraMenuPreset.COA_PlayerSettingsUI, 0, true);
+		COA_PlayerSettingsUI colorTeamMenu = COA_PlayerSettingsUI.Cast(menu);
+		
+		colorTeamMenu.SetPlayerInt(PlayerInt);
+	}
+	
+	protected void UpdatePlayerList()
+	{
+		if (!playersGroup) return;
+
+		string groupString = groupManager.ReturnGroupMapValue(playersGroup.ToString());
+
+		if (!groupString || groupString == "") return;
+
+		if (groupString == groupStringStored) return;
+		groupStringStored = groupString;
+
+		array<string> localGroupSplitString = {};
+		groupString.Split(";", localGroupSplitString, true);
+
+		int groupCount = localGroupSplitString.Count();
+		groupCount = groupCount/ 4;
+
+		int playerPlace = 0;
+
+		// Parse through current group array.
+		for (int i = 0; i<groupCount && i < 12; i++)
+		{
+			string playerName = localGroupSplitString[playerPlace];
+			string colorTeam = localGroupSplitString[playerPlace + 1];
+			string icon = localGroupSplitString[playerPlace + 2];
+
+			playerPlace = playerPlace + 4;
+
+			// Get group display widgets.
+			TextWidget playerDisplay = TextWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Player%1", i)));
+			ImageWidget statusDisplay = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", i)));
+
+			playerDisplay.SetText(playerName);
+			statusDisplay.SetOpacity(1);
+			statusDisplay.LoadImageTexture(0, icon);
+			
+			float sx = 0;
+			float yx = 0;
+			playerDisplay.GetTextSize(sx, yx);
+			
+			if (sx > 170) {
+			  	for (int e = 0; sx > 160; e++)
+				{
+					int playerNameLength = playerName.Length();
+					playerNameLength = playerNameLength - 1;
+					playerName = playerName.Substring(0, playerNameLength);
+					
+					//Players display wont visually update until the UpdatePlayerList function ends, so they don't see this.
+					playerDisplay.SetText(playerName);
+					playerDisplay.GetTextSize(sx, yx);
+				};
+				playerName = string.Format("%1......", playerName);
+				playerDisplay.SetText(playerName);
+			};
+			
+
+			if (colorTeam) {
+				switch (colorTeam)
+				{
+					case "Red"    : {statusDisplay.SetColorInt(ARGB(255, 200, 65, 65));   playerDisplay.SetColorInt(ARGB(255, 200, 65, 65));   break; };
+					case "Blue"   : {statusDisplay.SetColorInt(ARGB(255, 0, 92, 255));    playerDisplay.SetColorInt(ARGB(255, 0, 92, 255));    break; };
+					case "Yellow" : {statusDisplay.SetColorInt(ARGB(255, 230, 230, 0));   playerDisplay.SetColorInt(ARGB(255, 230, 230, 0));   break; };
+					case "Green"  : {statusDisplay.SetColorInt(ARGB(255, 0, 190, 85));    playerDisplay.SetColorInt(ARGB(255, 0, 190, 85));    break; };
+					case "None"   : {statusDisplay.SetColorInt(ARGB(255, 255, 255, 255)); playerDisplay.SetColorInt(ARGB(255, 255, 255, 255)); break; };
+				};
+			};
+		};
+		for (int e = groupCount; e<=12; e++)
+		{
+			// Get group display widgets.
+			TextWidget playerRemoveDisplay = TextWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Player%1", e)));
+			ImageWidget statusRemoveDisplay = ImageWidget.Cast(m_wRoot.FindAnyWidget(string.Format("Status%1", e)));
+
+			// Skip ahead to next for-loop iteration if either of these are false.
+			if (!playerRemoveDisplay || !statusRemoveDisplay) continue;
+
+			// Clear widgets.
+			playerRemoveDisplay.SetText("");
+			statusRemoveDisplay.SetOpacity(0);
+		}
+	};
+}
