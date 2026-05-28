fd9300f81e59e5bbc91f72bdc6256b565f9fe83c
Njpatman <77807058+Njpatman@users.noreply.github.com>
2026-05-28 00:44:10 -0400

    OCD is a helluva drug

--- Scripts/Game/Core/Helpers/CSI_CharacterHelper.c
+++ Scripts/Game/Core/Helpers/CSI_CharacterHelper.c
@@ -5,11 +5,9 @@ class CSI_CharacterHelper
 //=============================================================================================================================================================================================================================================================================================================================================================
 
 	//------------------------------------------------------------------------------------------------
-	
 	//! Get characters yaw axis value
 	//! \param[in] playerCharacter: The inputed players character entity
 	//! \return The yaw of the character entity
-	
 	static float GetCharacterYaw(SCR_ChimeraCharacter playerCharacter)
 	{
 		if (!playerCharacter)
@@ -38,11 +36,9 @@ class CSI_CharacterHelper
 	}
 
 	//------------------------------------------------------------------------------------------------
-	
 	//! Get characters current vehicle slot
 	//! \param[in] playerCharacterL The inputed players character entity
 	//! \return The compartment the entity is in (will return null if they aren't in a vehicle)
-	
 	static BaseCompartmentSlot GetCharacterVehicleCompartment(IEntity playerCharacter)
 	{
 		if (!playerCharacter)
